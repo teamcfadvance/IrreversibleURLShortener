258efcab79c7af88d20e465366ea07a9d0632163
Denard Springle <denard.springle@gmail.com>
2014-02-12 10:35:10 -0500

    Adding in the MySQL script for table generation

---
+++ iusDemo_MySQL.sql
@@ -0,0 +1,9 @@
+--
+-- Table structure for table `iusDemo`
+--
+
+CREATE TABLE IF NOT EXISTS `iusdemo` (
+  `uniqueId` int(11) NOT NULL AUTO_INCREMENT,
+  `demoId` varchar(50) COLLATE utf16_bin DEFAULT NULL,
+  PRIMARY KEY (`uniqueId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COLLATE=utf16_bin AUTO_INCREMENT=1 ;
